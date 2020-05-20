ec787156bb7bd7e9fd2ed2fcd5f59a248535d364
Prashant Gautam <prashant.gtm1001@gmail.com>
2020-05-20 23:20:36 +0530

    Xcode Project added

---
+++ FirstCIProject/FirstCIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstCIProject
+//
+//  Created by Prashant on 20/05/20.
+//  Copyright © 2020 Prashant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
